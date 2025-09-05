a1b2695c5473fbe221e59b9bbb84eea3a711467e
Marcos2D15 <68926895+Marcos2D15@users.noreply.github.com>
2025-09-05 00:32:54 +0000

    Atualização1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define TAM 2
+#define TAM 2 //Quantidade de cartas
 
 // Definição da Estrutura Carta.
 typedef struct{
@@ -9,10 +9,13 @@ typedef struct{
     char nomeCidade [50];              // Nome da Cidade
     int populacao;                     // Número de Habitantes
     float area;                       // Área em Km²
-    float pib;                        // Produto Interno Bruto
-    int pontosTuristicos;            // Quantidades de Pontos Turíscos 
+    float pib;                       // Produto Interno Bruto
+    int pontosTuristicos;           // Quantidades de Pontos Turíscos 
+    float densidadepopulacional;    // População / Área
+    float pibpercapita;            //Pib / população
 } Carta;
 
+//Função para Cadastrar uma carta
 void cadastrarCarta(Carta *carta, int numero){
     printf ("\n--- Cadastro da Carta %d--- \n", numero + 1);
     
@@ -20,7 +23,7 @@ void cadastrarCarta(Carta *carta, int numero){
     scanf(" %c", &carta->estado);
 
     printf("Código da Carta (Ex: A01): ");
-    scanf("%s",carta->codigo);
+    scanf("%s", carta->codigo);
 
     printf("Nome da Cidade: ");
     scanf(" %[^\n]", carta->nomeCidade);   // Lê string com espaços
@@ -36,8 +39,17 @@ void cadastrarCarta(Carta *carta, int numero){
     
     printf("Números de Pontos Turísticos: ");
     scanf("%d", &carta->pontosTuristicos);
-}
 
+    //---Cálculos Automáticos---
+    carta->densidadepopulacional = carta->populacao / carta->area;
+    carta->pibpercapita = (carta->pib *1000000000) / carta->populacao;
+
+    //Exibe os Cálculos no mesmo padão
+    printf("Densidade Populacional: %.2f hab/km²\n", carta->densidadepopulacional);
+    printf("Pib per Capita: %.2f reais\n", carta->pibpercapita);
+
+}
+//Função para Exibir Cartas
 void exibirCarta(Carta carta, int numero){
     printf("\n=== Informações da Carta %d ===\n", numero + 1);
     printf("Estado: %c\n", carta.estado);
@@ -46,11 +58,13 @@ void exibirCarta(Carta carta, int numero){
     printf("População: %d habitantes\n", carta.populacao);
     printf("Área: %.2f km² \n", carta.area);
     printf("Pib: R$ %.2f bilhões\n", carta.pib);
-    printf("Números de Pontos Turísticos: %d\n", carta.pontosTuristicos);    
+    printf("Números de Pontos Turísticos: %d\n", carta.pontosTuristicos);   
+    printf("Densidade Populacional: %.2f hab/km²\n", carta.densidadepopulacional);
+    printf("Pib per Capita: %.2f reais\n", carta.pibpercapita); 
 }
 
 int main () {
-    Carta cartas[TAM];
+    Carta cartas[TAM]; //Vetor de Cartas
 
     // Cadrasta as duas cartas
     for (int i = 0; i < TAM; i++) {
