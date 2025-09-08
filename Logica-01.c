42d7663fe1609a9aa6c6837ae83bfe0b0a776d29
Marcos2D15 <68926895+Marcos2D15@users.noreply.github.com>
2025-09-08 16:47:11 +0000

    logica02

--- Logica-01.c
+++ Logica-01.c
@@ -9,29 +9,58 @@ typedef struct {
 
 int main(){
     Carta c1,c2;
+    int escolha;
 
-    printf("Digite o nome da Cidade da Carta 1: ");
-    scanf("%[^\n]", c1.nomeCidade);
-    printf("Digite a População da carta 1: ");
+    printf("Digite o nome da cidade da carta 1: ");
+    scanf(" %[^\n]", c1.nomeCidade);
+    printf("População:");
     scanf("%lu", &c1.populacao);
+    printf("Área: ");
+    scanf("%f", &c1.area);
+    printf("Pib: ");
+    scanf("%f", &c1.pib);
 
-
-
-    printf("Digite o nome da Cidade da carta 2: ");
-    scanf("%[^\n]", c2.nomeCidade);
-    printf("Digite a População da carta 2: ");
+    printf("\nDigite o nome da Cidade da carta 2: ");
+    scanf(" %[^\n]", c2.nomeCidade);
+    printf("População: ");
     scanf("%lu", &c2.populacao);
+    printf("Área: ");
+    scanf("%f", &c2.area);
+    printf("Pib: ");
+    scanf("%f", &c2.pib);
 
-    printf("\nComparando cartas pelo atributo População...\n");
+    printf("\nEscolha o atributo para comparar:\n");
+    scanf("1 - populacao\n2 - area\n3 - pib\n");
+    scanf("%d", &escolha);
 
-    if (c1.populacao > c2.populacao) {
-        printf("%s Venceu!\n", c1.nomeCidade);
-    } else if (c2.populacao > c1.populacao) {
-        printf("%s Venceu!\n", c2.nomeCidade);
-    } else{
-        printf("Empate!\n");
+    switch (escolha) {
+        case 1:
+            if (c1.populacao > c2.populacao)
+                printf("%s venceu!\n", c1.nomeCidade);
+            else if (c2.populacao > c1.populacao)
+                printf("%s venceu!\n", c2.nomeCidade);
+            else
+                printf("Empate!\n");
+            break;
+        case 2:
+            if (c1.area > c2.area)
+                printf("%s venceu!\n", c1.nomeCidade);
+            else if (c2.area > c1.area)
+                printf("%s venceu!\n", c2.nomeCidade);
+            else
+                printf("Empate!\n");
+        case 3:
+            if (c1.pib > c2.pib)
+                printf("%s venceu!\n", c1.nomeCidade);
+            else if (c2.pib > c1.pib)
+                printf("%s venceu!\n",c2.nomeCidade);
+            else
+                printf("Emapate.\n");
+            break;
+        
+        default:
+            printf("Opção invalida.\n");
     }
-    
-    return 0;
-    
+
+   return 0;  
 }
