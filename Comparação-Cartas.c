3293995e67ee1cbcd916f9d42b2c738cc6bd78b4
Marcos2D15 <68926895+Marcos2D15@users.noreply.github.com>
2025-09-10 02:15:02 +0000

    Commit carta-comparação

---
+++ Comparação-Cartas.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef  struct {
+    char nome [50];
+    int populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
+    float densidadeDemografica;
+} Carta;
+
+int main(){
+    //Duas cartas já cadastradas(´Poderia vir do desafio anterior.
+    Carta carta1 = {"Brasil", 213000000, 8516000.0, 22000.0, 50, 213000000 / 8515000.0};
+    Carta carta2 = {"Argentina", 45000000, 2780000.0, 10000.0, 30, 45000000 / 2780000.0};
+    
+    int opcao;
+
+    printf("===Super Triunfo - Comparção de Cartas ===\n");
+    printf("1 - População\n");
+    printf("2 - Área\n");
+    printf("3 - Pib\n");
+    printf("4 - Pontos Turísticos\n");
+    printf("5 - Densidade Demográfica\n");
+    printf("Escolha o atributo para Comparar: ");
+    scanf("%d", &opcao);
+
+    printf("\nComparando %s x %s\n", carta1.nome, carta2.nome);
+
+    switch(opcao) {
+        case 1: //População
+            printf("Atributo População\n");
+            printf("%s: %d habitantes\n", carta1.nome, carta1.populacao);
+            printf("%s: %d habitantes\n", carta2.nome, carta2.populacao);
+
+            if (carta1.populacao > carta2.populacao); {
+                printf("Vencedor: %s\n", carta1.nome);
+            } else if(carta1.populacao < carta2.populacao){
+                printf("Vencedor: %s\n, carta2.nome");   
+            } else{
+                printf("Empate!\n");
+            }
+            break;
+        
+        case 2: //Área
+            printf("Atributo: Área\n");
+            printf("%s: %.2f km²\n", carta1.nome, carta1.area);
+            printf("%s: %.2f km²\n", carta2.nome, carta2.area);
+            
+            if (carta1.area > carta2.area); {
+                printf("Vencedor: %s\n", carta1.nome);
+            } else if (carta1.area < carta2.area) {
+                printf("Vencedor: %s\n", carta2.nome);
+            } else {
+                printf("Empate!\n");
+            }
+            break;
+        
+        case 3: //Pib
+            printf("Atributo: Pib");
+            printf("%s: %.2f bilhões U$\n", carta1.nome, carta1.pib);
+            printf("%s: %.2f bilhões u$\n", carta2.nome, carta2.pib);
+
+            if (carta1.pib > carta2.pib) {
+                printf("Vencedor: %s\n", carta1.nome);
+            } else if (carta1.pib < carta2.pib) {
+                printf("Vencedor: %s\n", carta2.nome);
+            } else {
+                printf("Empate!\n");
+            }
+           break;
+    
+        case 4: //Pontos Turísticos
+            printf("Atributo: Pontos Turísticos\n");
+            printf("%s: %d pontos\n", carta1.nome, carta1.pontosTuristicos);
+            printf("%s: %d pontos\n", carta2.nome, carta2.pontosTuristicos);
+
+            if (carta1.pontosTuristicos > carta2.pontosTuristicos) {
+                printf("Vencedor: %s\n", carta1.nome);
+            } else if (carta1.pontosTuristicos < carta2.pontoTuristicos) {
+                printf("Vencedor: %s\n", carta2nome);
+            } else {
+                printf("Empate!\n");
+            }
+            break;        
+    
+        case 5: // Densidade Demográfica (MENOR Vence)
+            printf("Atributo: Densidade Demográfica\n");
+            printf("%s: %.2f ha/km²\n", carta1.nome, carta1.densidadeDemografica);    
+            printf("%s: %.2f ha/km²\n", carta2.nome, carta2.densidadeDemografica);
+            
+            if (carta1.densidadeDemografica < carta2.densidadeDemografica) {
+                printf("Vencedor: %s\n", carta1.nome);
+            } else if (carta1.densidadeDemografica > carta2.densidadeDemografica) {
+                printf("Vencedor: %s\n", carta2.nome);
+            } else{
+                printf("Empate!\n");
+            }
+            break;
+
+        default:
+            printf("Opção invalida!\n");                        
+    }
+    
+    return 0;
+}
+
+
+
